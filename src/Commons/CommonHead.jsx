00327ddf33e86ce83cfd0a2b539e2cf272a2a160
Md Mubin <fontdevmubin@gmail.com>
2024-12-17 14:26:00 +0600

    Changes to be committed: 	modified:   src/Commons/CommonHead.jsx 	modified:   src/Commons/CommonUsers.jsx 	modified:   src/Components/ChatProfileBox/ChatProfileBox.css 	modified:   src/Components/Navbar/Navbar.css 	modified:   src/Components/Navbar/Navbar.jsx

--- src/Commons/CommonHead.jsx
+++ src/Commons/CommonHead.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const CommonHead = ({ commonHeadName }) => {
     return (
         <>
-            <h1 className='text-center font-brandFont font-semibold text-5xl text-textColor-lightMode dark:text-textColor-darkMode'>
+            <h1 className='text-center font-brandFont font-semibold text-3xl xl:text-5xl text-textColor-lightMode dark:text-textColor-darkMode'>
                 {commonHeadName}
                 <div className='w-[80%] m-auto h-[2px] bg-slate-300 dark:bg-[#707070] mt-2 '></div>
-                <div className='w-[100%] m-auto h-[2px] bg-slate-300 dark:bg-[#707070] mt-2 '></div>
+                <div className='w-full m-auto h-[2px] bg-slate-300 dark:bg-[#707070] mt-2 '></div>
             </h1>
         </>
     )
